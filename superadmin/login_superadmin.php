6d8f491a802943e64968eee4b787092edb2294d7
rnkx <rngkxxx@gmail.com>
2025-10-30 22:54:21 +0800

    make some changes

--- superadmin/login_superadmin.php
+++ superadmin/login_superadmin.php
@@ -1,9 +1,6 @@
 <?php include("db.php"); 
 session_start();
-if (isset($_SESSION['error'])) {
-    echo "<p style='color:red'>" . $_SESSION['error'] . "</p>";
-    unset($_SESSION['error']);
-}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
