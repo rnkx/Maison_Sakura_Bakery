15c04ed090433056ff28faef5c0f94bd7d9c0e42
rnkx <rngkxxx@gmail.com>
2025-10-31 22:52:53 +0800

    adjust the footer and always stick to the bottom

--- admin/customer_products.php
+++ admin/customer_products.php
@@ -7,8 +7,11 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'customer') {
     header("Location: login_customer.php");
     exit();
 }
+
 // Capture search keyword & category
 $search = $_GET['search'] ?? '';
+
+
 
 // Get customer_id from session 
 $users_id = $_SESSION['users_id'] ?? ($_SESSION['id'] ?? null);
